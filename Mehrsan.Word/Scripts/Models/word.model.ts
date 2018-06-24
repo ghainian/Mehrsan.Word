050c4390b76b49fc6e658613fd9c319b1cf0c44b
ghainian <ghainian@gmail.com>
2018-06-24 09:17:38 +0200

    after deleting angualr cli

---
+++ Mehrsan.Word/Scripts/Models/word.model.ts
@@ -0,0 +1,13 @@
+export class Word{
+	id: number;
+	targetWord: string;
+	isAmbiguous: boolean;
+	movieName: string;
+	isMovieSubtitle: boolean;
+	meaning: string;
+	startTime: Date;
+	endTime: string;
+	writtenByMe: boolean;
+
+	
+}
