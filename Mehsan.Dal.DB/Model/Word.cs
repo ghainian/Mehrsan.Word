1f29624a6fd39bbb76e1c4fb6a5420c993d2f9e1
ghainian <ghainian@gmail.com>
2018-07-14 09:04:26 +0200

    EntitiyFrameworkCore issues resolved

--- Mehsan.Dal.DB/Model/Word.cs
+++ Mehsan.Dal.DB/Model/Word.cs
@@ -3,7 +3,9 @@ namespace Mehrsan.Dal.DB
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
 
+    [Table("Word")]
     public partial class Word
     {
         public Word()
