0f54ce56638501b2d513298895f71b2b6bd20c1e
ghainian <ghainian@gmail.com>
2018-08-13 01:47:51 +0200

    Adding Meta data class for Word Entity

---
+++ Mehsan.Dal.DB/MetaDataClass/Word.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mehrsan.Dal.DB.MetaDataClass
+{
+    public partial class WordMetadata
+    {
+        [Required]
+        public string TargetWord { get; set; }
+
+        [Required]
+        public string Meaning { get; set; }
+    }
+
+    [MetadataType(typeof(WordMetadata))]
+    public partial class Word
+    {
+    }
+}
